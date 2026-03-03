a2de8825ee5a81d456323b650c470e9d4014f2ef
17benabdallah-hue <17benabdallah@gmail.com>
2026-03-03 23:52:26 +0100

    Update and rename .gitkeep to ReminderApp.tsx

---
+++ components/ReminderApp.tsx
@@ -0,0 +1,10 @@
+
+// components/ReminderApp.tsx
+export default function ReminderApp() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>تطبيق التذكيرات الذكي</h1>
+      <p>قيد التطوير...</p>
+    </div>
+  );
+}
