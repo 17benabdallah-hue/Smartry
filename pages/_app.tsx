14e02feb8efaaf9140cb9468305b7145344e2d80
17benabdallah-hue <17benabdallah@gmail.com>
2026-03-04 05:57:17 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../globals.css';
 import { LanguageProvider } from '../lib/LanguageContext';
+import { ErrorBoundary } from '../components/ErrorBoundary';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <LanguageProvider>
-      <Component {...pageProps} />
-    </LanguageProvider>
+    <ErrorBoundary>
+      <LanguageProvider>
+        <Component {...pageProps} />
+      </LanguageProvider>
+    </ErrorBoundary>
   );
 }
